c135546cf5eb9bd7fcebd874a216e684b10e9363
KleverKobold <alejm9494@gmail.com>
2022-12-04 19:24:33 -0300

    Binary, Hexa and Myself

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 function laCajaDePandora(numero) {
   // proximamente escribiremos codigo aqui
+
+  if (numero % 2 === 0) {
+    let binario = [];
+    while (numero > 0) {
+      binario.unshift(numero % 2);
+      numero = Math.floor(numero / 2);
+    }
+    return binario.join("");
+  }
+
+  if (numero % 2 !== 0) {
+    if (numero % 2 !== 0) {
+      let hexaString = numero.toString(16);
+      let hexaNumber = parseInt(hexaString);
+      return hexaNumber;
+    }
+  }
+}
+
+function myNameIs() {
+  let alejandro = {
+    name: "Alejandro",
+    edad: "28",
+    nacionalidad: "Argentino",
+  };
+  return alejandro;
 }
